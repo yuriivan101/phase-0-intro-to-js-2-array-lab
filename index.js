6cb7221b36419f79e67deb19b14c232594be15f4
Yuriy <ivanenko.yuri@gmail.com>
2024-02-19 08:38:56 -0500

    Passed all 9 array tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = [...cats,name]
+    return newCats
+}
+
+function prependCat(name){
+    const newCats = [name,...cats]
+    return newCats
+}
+
+function removeLastCat(){
+    const newCats = cats.slice(0,-1)
+    //console.log(newCats)
+    return newCats
+}
+//removeLastCat()
+
+function removeFirstCat(){
+    const newCats = cats.slice(1)
+    return newCats
+}
